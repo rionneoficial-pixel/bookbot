b4ffd989560e59186359c964d99717e6cf1b89b5
Rion_neoficial <rionn@Lovezz.localdomain>
2025-10-15 10:10:12 -0300

    a description of your changes here

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from stats import contador, silabador, lista_dicio
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    path = "books/frankenstein.txt"
+    path = sys.argv[1]
     texto = get_book_text(path)
     numbers = contador(texto)
     dicio = silabador (texto)
